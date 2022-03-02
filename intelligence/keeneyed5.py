c09e0e557b6502f427574e426a993f0f743b38d8
Kedev <80370389+cs-AI-dev@users.noreply.github.com>
2022-03-02 15:28:05 -0600

    Update keeneyed5.py

--- intelligence/keeneyed5.py
+++ intelligence/keeneyed5.py
@@ -9,6 +9,55 @@ cli.display("[KE5:RT] Connecting subroutine driver to CLI ...")
 subroutine.connectCLI(cli)
 cli.display("[KE5:RT] Done.")
 
+shorttermMemorySystem = None
+
 def acceptInput(inp):
     # Commit to short-term memory
     pass
+
+def initialize():
+	global shorttermMemorySystem
+	
+	cli.display("[KE5:RT:INIT] Starting up Keeneyed-5 artificial general intelligence system ...\n")
+	cli.display("    Checking short-term memory subsystem ...")
+	shorttermMemorySystem = subroutine.srt_01()
+	status = True
+	for x in shorttermMemorySystem.all:
+		if not x:
+			status = False
+	if not status:
+		cli.display(cli.term.yellow_on_black("    WARN: Anomaly detected in short-term memory caches. Resetting ..."))
+		subroutine.srt_02()
+		cli.display("    Short-term memory caches reset. Reloading ...")
+		shorttermMemorySystem = subroutine.srt_01()
+		cli.display("    Reload complete. Checking subsystem again ...")
+		status = True
+		for x in shorttermMemorySystem.all:
+			if not x:
+				status = False
+		if not status:
+			cli.display(cli.term.red_on_black("    ERROR: Short-term memory subsystem failure. Continue? (y/n)"))
+			with term.cbreak(), term.hidden_cursor():
+				usr = None
+				while True:
+					inkey = cli.term.inkey()
+					if inkey.lower() == "y":
+						usr = True
+						break
+					elif inkey.lower() == "n":
+						usr = False
+						break
+					else:
+						pass
+				if usr == False:
+					cli.display("    Short-term memory subsystem error detected, user override activated. Continuing ...")
+				else:
+					cli.display("        Exiting ...")
+					return 0
+	else:
+		cli.display("    Short-term memory subsystem intact and functional. Continuing ...")
+	
+	cli.display("    Creating long-term memory cache ...")
+	subroutine.srt_03()
+	
+	
