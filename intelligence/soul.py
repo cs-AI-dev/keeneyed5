44a0071f440cab94f2e1490252fe7530e2ba0c06
Kedev <80370389+cs-AI-dev@users.noreply.github.com>
2022-03-07 13:06:41 -0600

    Update soul.py

--- intelligence/soul.py
+++ intelligence/soul.py
@@ -2,25 +2,22 @@ import sys
 import random
 import datetime
 import os
-os.system("py -m pip install nltk -q")
-os.system("py -m pip install tk -q")
+os.system("py -m pip install nltk -q") # Ensure NLTK is installed
+os.system("py -m pip install tk -q")   # Ensure TKinter is installed
 
 import nltk
-from nltk.corpus import wordnet as wn
-from nltk.corpus import brown as browncorpus
-from nltk.corpus import words as wordtk
-import tkinter as tk
-from tkinter import Tk
-from tkinter import Label
-from tkinter import Frame
-from tkinter import Entry
-from tkinter import Button
+from nltk.corpus import wordnet as wn        # WordNet language tree parser
+from nltk.corpus import brown as browncorpus # Brown Corpus
+from nltk.corpus import words as wordtk      # Word tokenizer
+import tkinter as tk						 # TKinter GUI
+from tkinter import Tk						 # TK Window
+from tkinter import Label					 # TK Label
+from tkinter import Frame					 # TK Frame
+from tkinter import Entry					 # TK Entry
+from tkinter import Button					 # TK Button
 
-import memory
-import subroutine
-
-def sendData():
-	pass
+import memory								 # Memory subsystem
+import subroutine							 # Subroutines
 
 class gui:
 	bg = "black"
@@ -36,8 +33,14 @@ def add(name, element):
 	except Exception as e:
 		return e
 
-def assembleGUI():
+def assemble(intelligenceName):
+	# Startup
+	subroutine.srt_01()
+	subroutine.srt_02()
+	subroutine.srt_03()
+	
 	window = Tk()
+	window.configure(bg=gui.bg)
 
 	add("titlelbl", Label(window, text="Keeneyed-5 Artificial General Intelligence System", bg=gui.bg, fg=gui.fg, font=gui.font(24)))
 	elements["titlelbl"].grid(row=1, column=1, columnspan=3)
@@ -50,9 +53,23 @@ def assembleGUI():
 
 	add("interface", Label(elements["center"], text="", bg=gui.bg, fg=gui.fg, font=gui.font(12), height=25, width=100))
 	elements["interface"].grid(row=1, column=1, columnspan=3)
+	
+	def post(usr, data):
+		elements["interface"].config(text = f"[{usr}] {data}")
+	
 	add("datainput", Entry(elements["center"], text="", bg=gui.bg, fg=gui.fg, font=gui.font(12), width=70))
 	elements["datainput"].grid(row=2, column=1, columnspan=1)
-	add("sendbutton", Button(elements["center"], text="INPUT DATA", bg="lime", fg="black", font=gui.font(12), width=10, command=sendData))
+	
+	def receive():
+		post("USER", elements["datainput"].get())
+		memory.record.shortterm([x + "." for x in elements["datainput"].get().split(".")])
+		output = "..."
+		
+		# KE5 process
+		
+		post(intelligenceName, output)
+	
+	add("sendbutton", Button(elements["center"], text="INPUT DATA", bg="lime", fg="black", font=gui.font(12), width=10, command=receive))
 	elements["sendbutton"].grid(row=2, column=2, columnspan=1)
 	add("terminateconnectionbutton", Button(elements["center"], text="TERMINATE CONNECTION", bg="red", fg="black", font=gui.font(12), width=20, command=window.destroy))
 	elements["terminateconnectionbutton"].grid(row=2, column=3, columnspan=1)
